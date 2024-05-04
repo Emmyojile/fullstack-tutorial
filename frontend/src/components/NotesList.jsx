c886580b9dfebab267211a538a2568e8bc927b0c
Emmyojile <emmaojile99@gmail.com>
2024-05-04 09:57:17 +0100

    pushing to github without env and conditionally rendering notes

--- frontend/src/components/NotesList.jsx
+++ frontend/src/components/NotesList.jsx
@@ -3,22 +3,26 @@ import React from "react";
 const NoteList = ({ notes, handleDelete, handleEditClick }) => {
   return (
     <div className="card-container">
-      {notes.map((note,index) => (
-        <div className="card" key={index}>
-          <div className="content">
-            <span className="title">{note.title}</span>
-            <p className="message">{note.content}</p>
+      {notes.length ? ( // Check if notes array has elements
+        notes.map((note, index) => (
+          <div className="card" key={index}>
+            <div className="content">
+              <span className="title">{note.title}</span>
+              <p className="message">{note.content}</p>
+            </div>
+            <div className="button">
+              <button className="btn edit" onClick={() => handleEditClick(note)}>
+                Edit
+              </button>
+              <button className="btn delete" onClick={() => handleDelete(note._id)}>
+                Delete
+              </button>
+            </div>
           </div>
-          <div className="button">
-            <button className="btn edit" onClick={() => handleEditClick(note)}>
-              Edit
-            </button>
-            <button className="btn delete" onClick={() => handleDelete(note._id)}>
-              Delete
-            </button>
-          </div>
-        </div>
-      ))}
+        ))
+      ) : (
+        <h2>No notes yet! <br /> Create one above</h2> // Display message if notes is empty
+      )}
     </div>
   );
 };
